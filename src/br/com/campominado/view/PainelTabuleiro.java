f572473726a48e96b94f21c7ee8eea19632cbad3
Junior Almeida <jrjunioralmeida@outlook.com>
2025-08-20 16:26:09 -0300

    Criação da classe TelaPrincipal e suas funcionalidades e classe PainelTabuleiro

---
+++ src/br/com/campominado/view/PainelTabuleiro.java
@@ -0,0 +1,12 @@
+package br.com.campominado.view;
+
+import br.com.campominado.model.Tabuleiro;
+
+import javax.swing.*;
+
+public class PainelTabuleiro extends JPanel {
+
+    public PainelTabuleiro(Tabuleiro tabuleiro) {
+    
+    }
+}
