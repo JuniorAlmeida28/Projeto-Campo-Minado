ba4fa9872930e2523fa9adc8900c87427d611040
Junior Almeida <jrjunioralmeida@outlook.com>
2025-08-20 16:43:45 -0300

    Implementação das funcionalidade na classe PainelTabuleiro e Tabuleiro para exibir os botões

---
+++ src/br/com/campominado/view/BotaoCampo.java
@@ -0,0 +1,12 @@
+package br.com.campominado.view;
+
+import br.com.campominado.model.Campo;
+
+import javax.swing.*;
+
+public class BotaoCampo extends JButton {
+
+    public BotaoCampo(Campo campo) {
+
+    }
+}
--- src/br/com/campominado/view/PainelTabuleiro.java
+++ src/br/com/campominado/view/PainelTabuleiro.java
@@ -3,10 +3,16 @@ package br.com.campominado.view;
 import br.com.campominado.model.Tabuleiro;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PainelTabuleiro extends JPanel {
 
     public PainelTabuleiro(Tabuleiro tabuleiro) {
-    
+        setLayout(new GridLayout(tabuleiro.getLinhas(), tabuleiro.getColunas()));
+
+        tabuleiro.paraCadaCampo(c -> add(new BotaoCampo(c)));
+        tabuleiro.registarObservador(e ->{
+            //TODO mostar resultado pro usuário
+        });
     }
 }
